a58296f6c704c710aab3afdec8d8ad2d50144f52
fhub-1 <flutterhub1.21.0@gmail.com>
2022-09-09 16:28:40 +0300

    adding aboout and footer section

--- pages/index.js
+++ pages/index.js
@@ -1,52 +1,55 @@
-import Head from 'next/head';
-import Image from 'next/image';
-import Article from '../components/article';
-import TitleLinkSection from '../components/titleLinkSection';
-import Work from '../components/work';
+import Head from "next/head";
+import Image from "next/image";
+import Article from "../components/article";
+import TitleLinkSection from "../components/titleLinkSection";
+import Work from "../components/work";
 
 export default function Home() {
   return (
     <>
-      <div className='flex py-12 items-center container mx-auto'>
-        <div className='flex-1'>
-          <h1 className='text-2xl font-bold mb-4'>
-            Hi, I'm John!
+      <div className="flex py-12 items-center container mx-auto">
+        <div className="flex-1">
+          <h1 className="text-2xl font-bold mb-4">
+            Hi, I'm Joseph kitheka!
             <br />
-            Creative Technologist
+            Front-end React Developer
           </h1>
-          <p className='mb-4'>
-            Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
-            sint. Velit officia consequat duis enim velit mollit. Exercitation
-            veniam consequat sunt nostrud amet.
+          <p className="mb-4 ">
+            I am a Front - end React Developer with over 3 years of experience
+            in the industry. I am passionate about building products that people
+            love using the latest technologies. I am also an open source lover
+            and I enjoy contributing to projects that are helping to make the
+            world a better place.I love to create videos about my experiences
+            with different tools and technologies.
           </p>
           <a
-            href='#'
-            className='inline-block bg-pink-500 text-lg rounded-lg px-6 py-3 text-white'
+            href="#"
+            className="inline-block bg-primary text-lg rounded-lg px-6 py-3 text-white"
           >
             Check my Work
           </a>
         </div>
-        <div className='m-5 inline-block before:block before:absolute before:-left-2 before:-bottom-2 before:bg-pink-500 relative before:block before:absolute before:inset-0 before:bg-pink-500 relative z-0 before:rounded-full'>
+        <div className="m-5 inline-block before:block before:-left-2 before:-bottom-2 before:absolute before:inset-0 before:bg-primary relative z-0 before:rounded-full">
           <img
-            src='https://via.placeholder.com/240'
-            width='240'
-            height='240'
-            alt='Image of John'
-            className='z-10 relative rounded-full'
+            src="https://via.placeholder.com/240"
+            width="240"
+            height="240"
+            alt="Image of John"
+            className="z-10 relative rounded-full"
           />
         </div>
       </div>
-      <div className='bg-blue-100'>
-        <div className='container mx-auto py-12'>
-          <TitleLinkSection title={'Recent Posts'} />
-          <div className='grid grid-cols-2 gap-6'>
-            <Article className='rounded-lg' />
-            <Article className='rounded-lg' />
+      <div className="bg-blue-100">
+        <div className="container mx-auto py-12">
+          <TitleLinkSection title={"Recent Posts"} />
+          <div className="grid grid-cols-2 gap-6">
+            <Article className="rounded-lg" />
+            <Article className="rounded-lg" />
           </div>
         </div>
       </div>
-      <div className='container mx-auto py-12'>
-        <TitleLinkSection title={'Featured Work'} />
+      <div className="container mx-auto py-12">
+        <TitleLinkSection title={"Featured Work"} />
         <Work />
         <Work />
       </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#FF6464",
+        secondary: "#00A8CC",
+        dark: "#21243D",
+        light: "8695A4",
+      },
+    },
   },
   plugins: [],
 };
